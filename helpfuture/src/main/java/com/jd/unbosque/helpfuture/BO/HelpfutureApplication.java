762b6b844c4cd2f60c5c9dd8831bf9b010434f66
danielaZamb <danielaZamb>
2021-11-20 20:24:30 -0500

    DAO

---
+++ helpfuture/src/main/java/com/jd/unbosque/helpfuture/BO/HelpfutureApplication.java
@@ -0,0 +1,13 @@
+package com.jd.unbosque.helpfuture.BO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpfutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpfutureApplication.class, args);
+	}
+
+}
