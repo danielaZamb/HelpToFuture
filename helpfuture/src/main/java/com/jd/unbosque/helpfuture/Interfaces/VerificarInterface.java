21fc64b6c7466acb51994bd1227524b6d9c52318
jennyZam <jennyZam>
2021-11-20 20:37:20 -0500

    verificar

---
+++ helpfuture/src/main/java/com/jd/unbosque/helpfuture/Interfaces/VerificarInterface.java
@@ -0,0 +1,11 @@
+package com.jd.unbosque.helpfuture.Interfaces;
+
+public interface VerificarInterface {
+
+	public long verificarU(String user, String pass);
+	
+	public long verificarE(String user, String pass);
+	
+	public long verificarI(String user, String pass);
+
+}
